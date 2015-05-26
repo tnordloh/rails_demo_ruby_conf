c4bfbf4064d08ab51d646c1270f17ac4d98c6ff2
Timothy Nordloh <tnordloh@gmail.com>
2015-05-26 13:37:26 -0600

    added submission validation

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,3 +1,5 @@
 class Talk < ActiveRecord::Base
   belongs_to :track
+  validates :title, :name, :abstract, :notes, :email, :bio, presence: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
